a04ecb0a23651c456c5d9bb59ce0764d48524903
Puja kumari <puja19012005@gmail.com>
2026-04-13 23:38:18 +0530

    Create HelloWorld class with main method

---
+++ MyWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    // The main method is where the program starts execution
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
